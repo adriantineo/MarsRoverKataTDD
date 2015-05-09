5b05e0c2e94c881a12192f77d2658f6221aa8314
Adrian Tineo <me@adriantineo.com>
2015-05-09 12:07:31 +0200

    Remove duplication

--- app/mars_rover.py
+++ app/mars_rover.py
@@ -4,7 +4,6 @@ class MarsRover(object):
     def __init__(self):
         self.x = 0
         self.y = 0
-        self.position = (self.x, self.y)
         self.orientation = "N"
 
     def getPosition(self):
