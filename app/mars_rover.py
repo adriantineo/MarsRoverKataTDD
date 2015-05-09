88963cee804e6a7bfe51317b1096cba64c552714
Adrian Tineo <me@adriantineo.com>
2015-05-09 22:49:39 +0200

    Pass test sequence FFRFF and refactor movement

--- app/mars_rover.py
+++ app/mars_rover.py
@@ -20,11 +20,41 @@ class MarsRover():
     def turnLeft(self):
         self.orientationIdx = (self.orientationIdx - 1) % len(self.orientationVector)
 
-    def moveF(self):
-        if self.y < self.grid.getSize()[1] -1:
+    def _incrementY(self):
+        if self.getPosition()[1] < self.grid.getSize()[1] - 1:
             self.y += 1
         else:
             self.y = 0
+
+    def _decrementY(self):
+        if self.getPosition()[1] > 0:
+            self.y -= 1
+        else:
+            self.y = self.grid.getSize()[1] - 1
+
+    def _incrementX(self):
+        if self.getPosition()[0] < self.grid.getSize()[0] - 1:
+            self.x += 1
+        else:
+            self.x = 0
+
+    def _decrementX(self):
+        if self.getPosition()[0] > 0:
+            self.x -= 1
+        else:
+            self.x = self.grid.getSize()[0] - 1
+
+    def moveF(self):
+        if self.getOrientation() == "N":
+            self._incrementY()
+        elif self.getOrientation() == "E":
+            self._incrementX()
+        elif self.getOrientation() == "S":
+            self._decrementY()
+        elif self.getOrientation() == "W":
+            self._decrementX()
+        else:
+            raise Exception("Unknown orientation: {0}".format(self.getOrientation()))
 
     def getGrid(self):
         return self.grid
